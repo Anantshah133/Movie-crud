d45e15e2a5eebc510f8f3a72310e9f0731a19a7c
Anantshah133 <anantshah130305@gmail.com>
2024-03-14 01:07:34 +0530

    Delete record with image functionality complete

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -35,9 +35,54 @@ const getEditMovie = async (req, res) => {
     const movie = await Movie.findOne({ _id: id });
     res.render("edit-movie.ejs", { movie, mode: 'edit' });
 }
+const updateMovie = async (req, res) => {
+    try {
+        const { title, description, genere, ratings, release_date, id, old_image, old_video } = req.body;
+        let imageFilename = old_image;
+        let videoFilename = old_video;
 
+        if (req.files['movie-image'] && req.files['movie-image'][0]) 
+            imageFilename = req.files['movie-image'][0].filename;
+        if (req.files['movie-video'] && req.files['movie-video'][0]) 
+            videoFilename = req.files['movie-video'][0].filename;
+        
+        let result = await Movie.findOneAndUpdate(
+            { _id: id },
+            { title, description, genere, ratings, release_date, image: imageFilename, video: videoFilename },
+            { new: true }
+        );
+
+        if (req.files['movie-image'] && req.files['movie-image'][0]) deleteFile(old_image);
+        if (req.files['movie-video'] && req.files['movie-video'][0]) deleteFile(old_video);
+
+        res.cookie("msg", "update");
+        res.redirect("/movies");
+    } catch (error) {
+        console.log("Some error occurred: " + error);
+        res.status(500).send("Internal Server Error");
+    }
+}
+const deleteMovie = async (req, res)=>{
+    try {
+        const id = req.params.id;
+        const movieObj = await Movie.findOne({_id: id});
+        const result = await Movie.findByIdAndDelete(id);
+
+        if(result) {
+            deleteFile(movieObj.image);
+            deleteFile(movieObj.video);
+        }
+        
+        res.cookie("msg", "delete");
+        res.redirect("/movies");
+    } catch (error) {
+        console.log(`Error while deleting record - ${error}`);
+    }
+}
 module.exports = {
-    createMovie,
     getAllMovies,
     getEditMovie,
+    createMovie,
+    updateMovie,
+    deleteMovie,
 }
--- src/routes/movie.js
+++ src/routes/movie.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { Movie } = require("../models/Movie");
 const router = express.Router();
 const { upload } = require("../utils/multer");
-const { createMovie, getAllMovies, getEditMovie } = require("../controllers/movie");
+const { createMovie, getAllMovies, getEditMovie, updateMovie, deleteMovie } = require("../controllers/movie");
 const { deleteFile } = require("../utils/delete-file");
 
 router.get("/", (req, res) => {
@@ -15,45 +15,7 @@ router.get("/add", (req, res) => {
 
 router.get("/get-movies", getAllMovies);
 router.get("/edit/:id", getEditMovie);
+router.get("/delete/:id", deleteMovie);
 router.post("/add", upload.fields([{ name: 'movie-image', mamaxCount: 1 }, { name: 'movie-video', mamaxCount: 1 }]), createMovie)
-
-router.post("/update", upload.fields([{ name: 'movie-image', mamaxCount: 1 }, { name: 'movie-video', mamaxCount: 1 }]), async (req, res) => {
-    try {
-        const { title, description, genere, ratings, release_date, id, old_image, old_video } = req.body;
-        let imageFilename = old_image;
-        let videoFilename = old_video;
-
-        if (req.files['movie-image'] && req.files['movie-image'][0]) {
-            console.log("New image uploaded:", req.files['movie-image'][0].filename);
-            imageFilename = req.files['movie-image'][0].filename;
-        }
-        if (req.files['movie-video'] && req.files['movie-video'][0]) {
-            console.log("New video uploaded:", req.files['movie-video'][0].filename);
-            videoFilename = req.files['movie-video'][0].filename;
-        }
-
-        // console.log("Deleting old image:", old_image);
-        // console.log("Deleting old video:", old_video);
-
-        let result = await Movie.findOneAndUpdate(
-            { _id: id },
-            { title, description, genere, ratings, release_date, image: imageFilename, video: videoFilename },
-            { new: true }
-        );
-
-        if (req.files['movie-image'] && req.files['movie-image'][0]) {
-            console.log("Called");
-            deleteFile(old_image);
-        }
-        if (req.files['movie-video'] && req.files['movie-video'][0]) {
-            deleteFile(old_video);
-        }
-
-        res.cookie("msg", "update");
-        res.redirect("/movies");
-    } catch (error) {
-        console.log("Some error occurred: " + error);
-        res.status(500).send("Internal Server Error");
-    }
-});
+router.post("/update", upload.fields([{ name: 'movie-image', mamaxCount: 1 }, { name: 'movie-video', mamaxCount: 1 }]), updateMovie);
 module.exports = router;
